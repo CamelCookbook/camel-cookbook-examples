1cf2592fef87caea532b7a9340fede071d87a82e
jkorab <jakub.korab@gmail.com>
2013-01-22 23:25:24 +0000

    Added tests for changing the MEP from InOut to InOnly within a route.

---
+++ camel-cookbook-routing/src/test/java/org/camelcookbook/routing/changingmep/InOutCallingInOnlyTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) Scott Cranton and Jakub Korab
+ * https://github.com/CamelCookbook
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.camelcookbook.routing.changingmep;
+
+import org.apache.camel.*;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.camelcookbook.routing.recipientlist.RecipientListRouteBuilder;
+import org.junit.Test;
+
+public class InOutCallingInOnlyTest extends CamelTestSupport {
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new InOutCallingInOnlyRouteBuilder();
+    }
+
+    @Produce(uri = "direct:in")
+    protected ProducerTemplate template;
+
+    @EndpointInject(uri = "mock:oneWay")
+    private MockEndpoint oneWay;
+
+    @EndpointInject(uri = "mock:afterOneWay")
+    private MockEndpoint afterOneWay;
+
+    @Test
+    public void testMEPChangedForOneWay() throws InterruptedException {
+        String messageBody = "message";
+
+        oneWay.setExpectedMessageCount(1);
+        oneWay.message(0).exchangePattern().isEqualTo(ExchangePattern.InOnly);
+        afterOneWay.setExpectedMessageCount(1);
+        afterOneWay.message(0).exchangePattern().isEqualTo(ExchangePattern.InOut);
+
+        template.requestBody(messageBody);
+
+        assertMockEndpointsSatisfied();
+        Exchange oneWayExchange = oneWay.getReceivedExchanges().get(0);
+        Exchange afterOneWayExchange = afterOneWay.getReceivedExchanges().get(0);
+        // these are not the same exchange objects
+        assertNotEquals(oneWayExchange, afterOneWayExchange);
+
+        // the bodies should be the same - shallow copy
+        assertEquals(oneWayExchange.getIn().getBody(), afterOneWayExchange.getIn().getBody());
+    }
+
+}

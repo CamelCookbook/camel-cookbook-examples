0ea4e824f4ffe8b453918ec827af2efce4954de4
Scott Cranton <scott@cranton.com>
2013-03-03 08:46:50 -0500

    Added xmljson example

---
+++ camel-cookbook-transformation/src/main/java/org/camelcookbook/transformation/xmljson/XmlJsonRouteBuilder.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) Scott Cranton and Jakub Korab
+ * https://github.com/CamelCookbook
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.camelcookbook.transformation.xmljson;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.dataformat.xmljson.XmlJsonDataFormat;
+
+import java.util.Arrays;
+
+
+public class XmlJsonRouteBuilder extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        XmlJsonDataFormat xmlJsonFormat = new XmlJsonDataFormat();
+        xmlJsonFormat.setRootName("bookstore");
+        xmlJsonFormat.setElementName("book");
+        xmlJsonFormat.setExpandableProperties(Arrays.asList("author", "author"));
+
+        from("direct:marshal")
+            .marshal(xmlJsonFormat)
+            .to("mock:marshalResult");
+
+        from("direct:unmarshal")
+            .unmarshal(xmlJsonFormat)
+            .to("mock:unmarshalResult");
+    }
+}

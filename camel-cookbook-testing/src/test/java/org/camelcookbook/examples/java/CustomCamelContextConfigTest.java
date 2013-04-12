8b91c11787cc1f757e2b925a804c7f556f1378a8
Jakub Korab <jakub.korab@gmail.com>
2013-04-12 19:13:24 +0100

    Added custom CamelContext configuring test. Downgraded ActiveMQ due to 5.8.0 conflicting with the camel-jms version in 2.10.4.

---
+++ camel-cookbook-testing/src/test/java/org/camelcookbook/examples/java/CustomCamelContextConfigTest.java
@@ -0,0 +1,51 @@
+package org.camelcookbook.examples.java;
+
+import org.apache.activemq.camel.component.ActiveMQComponent;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Component;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.camel.builder.RouteBuilder;
+import org.junit.Test;
+
+/**
+ * @author jkorab
+ */
+public class CustomCamelContextConfigTest extends CamelTestSupport {
+    
+    @Override
+    public CamelContext createCamelContext() {
+        CamelContext context = new DefaultCamelContext();
+        ActiveMQComponent activeMQComponent = new ActiveMQComponent();
+        activeMQComponent.setBrokerURL("vm:localhost?broker.persistent=false");
+        context.addComponent("activemq", activeMQComponent);
+        return context;
+    }
+
+    @Override
+    public RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
+            @Override
+            public void configure() {
+                from("direct:in")
+                    .to("activemq:orders");
+
+                from("activemq:orders")
+                    .to("mock:out");
+            }
+        };
+    }
+
+    @Test
+    public void testMessagesFlowOverQueue() throws InterruptedException {
+        MockEndpoint out = getMandatoryEndpoint("mock:out", MockEndpoint.class);
+        out.setExpectedMessageCount(1);
+
+        template.sendBody("direct:in", "hello");
+
+        assertMockEndpointsSatisfied();
+        out.expectedBodiesReceived("hello");
+    }
+
+}

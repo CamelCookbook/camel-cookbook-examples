984a5fb2428bb8f4f9dbbdf9fc3beba1c0ef29dd
Scott Cranton <scott@cranton.com>
2013-05-06 06:07:03 -0400

    Added Type Converter example

---
+++ camel-cookbook-extend/src/main/java/org/camelcookbook/extend/datatype/MyPerson.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) Scott Cranton and Jakub Korab
+ * https://github.com/CamelCookbook
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.camelcookbook.extend.datatype;
+
+public class MyPerson {
+    private String firstName;
+    private String lastName;
+
+    public MyPerson() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MyPerson myPerson = (MyPerson) o;
+
+        if (firstName != null ? !firstName.equals(myPerson.firstName) : myPerson.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(myPerson.lastName) : myPerson.lastName != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "MyPerson{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}

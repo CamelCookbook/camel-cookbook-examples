03fd108577c09592ff227700de9169a445486fbc
Scott Cranton <scott@cranton.com>
2013-09-25 07:00:13 -0400

    Added operation routing example

---
+++ camel-cookbook-web-services/src/test/java/org/camelcookbook/ws/operation/OperationSpringTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) Scott Cranton and Jakub Korab
+ * https://github.com/CamelCookbook
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.camelcookbook.ws.operation;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.camel.test.AvailablePortFinder;
+import org.apache.camel.test.spring.CamelSpringTestSupport;
+import org.camelcookbook.ws.payment_service_v2.FaultMessage;
+import org.camelcookbook.ws.payment_service_v2.Payment;
+import org.camelcookbook.ws.payment_service_v2.PaymentService;
+import org.camelcookbook.ws.payment_service_v2.types.CheckStatusRequest;
+import org.camelcookbook.ws.payment_service_v2.types.CheckStatusResponse;
+import org.camelcookbook.ws.payment_service_v2.types.TransferRequest;
+import org.camelcookbook.ws.payment_service_v2.types.TransferResponse;
+import org.junit.Test;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class OperationSpringTest extends CamelSpringTestSupport {
+    private final int port1 = AvailablePortFinder.getNextAvailable();
+
+    @Override
+    protected AbstractApplicationContext createApplicationContext() {
+        System.setProperty("port1", String.valueOf(port1));
+
+        return new ClassPathXmlApplicationContext("META-INF/spring/ws-operation.xml");
+    }
+
+    @Test
+    public void testOperationSpring() throws MalformedURLException, FaultMessage {
+        final String wsdlUrl = String.format("http://localhost:%d/paymentServicev2?WSDL", port1);
+
+        final Payment paymentService = new PaymentService(new URL(wsdlUrl)).getPaymentPort();
+
+        TransferRequest transferRequest = new TransferRequest();
+        transferRequest.setBank("Bank of Camel");
+        transferRequest.setFrom("Jakub");
+        transferRequest.setTo("Scott");
+        transferRequest.setAmount("1");
+
+        TransferResponse transferResponse = paymentService.transferFunds(transferRequest);
+
+        assertNotNull(transferResponse);
+        assertEquals("OK", transferResponse.getReply());
+
+        CheckStatusRequest checkStatusRequest = new CheckStatusRequest();
+        checkStatusRequest.setId(transferResponse.getId());
+        checkStatusRequest.setBank("Bank of Camel");
+        checkStatusRequest.setFrom("Jakub");
+
+        CheckStatusResponse checkStatusResponse = paymentService.checkStatus(checkStatusRequest);
+
+        assertNotNull(checkStatusResponse);
+        assertEquals("Complete", checkStatusResponse.getStatus());
+    }
+}

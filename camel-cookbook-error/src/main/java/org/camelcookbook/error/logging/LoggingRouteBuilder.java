3a2c3eb476c394b9281454ff3e26bdfa46327fd6
Scott Cranton <scott@cranton.com>
2013-11-06 06:50:47 -0500

    included logging level attribute

--- camel-cookbook-error/src/main/java/org/camelcookbook/error/logging/LoggingRouteBuilder.java
+++ camel-cookbook-error/src/main/java/org/camelcookbook/error/logging/LoggingRouteBuilder.java
@@ -17,18 +17,24 @@
 
 package org.camelcookbook.error.logging;
 
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.camelcookbook.error.shared.FlakyProcessor;
 
 public class LoggingRouteBuilder extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-        errorHandler(loggingErrorHandler().logName("MyLoggingErrorHandler"));
+        errorHandler(loggingErrorHandler()
+            .logName("MyLoggingErrorHandler")
+            .level(LoggingLevel.ERROR)
+        );
 
         from("direct:start").bean(FlakyProcessor.class).to("mock:result");
 
         from("direct:routeSpecific")
-            .errorHandler(loggingErrorHandler().logName("MyRouteSpecificLogging"))
+            .errorHandler(loggingErrorHandler()
+                .logName("MyRouteSpecificLogging")
+                .level(LoggingLevel.ERROR))
             .bean(FlakyProcessor.class)
             .to("mock:result");
     }

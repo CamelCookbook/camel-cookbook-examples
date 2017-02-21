d0d51b1bb0db5e6fc5b8faae28f3b88348654126
Scott Cranton <scott@cranton.com>
2017-02-21 07:26:28 -0500

    Updated copyright

--- camel-cookbook-runtimes/runtime-vertx/src/test/java/com/camelcookbook/runtimes/vertx/VerticleTest.java
+++ camel-cookbook-runtimes/runtime-vertx/src/test/java/com/camelcookbook/runtimes/vertx/VerticleTest.java
@@ -1,13 +1,13 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+/*
+ * Copyright (C) Scott Cranton, Jakub Korab, and Christian Posta
+ * https://github.com/CamelCookbook
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -35,7 +35,7 @@ import org.junit.runner.RunWith;
 public class VerticleTest {
 
     private Vertx vertx;
-    private int port = 8081;
+    private int port = 8080; // TODO: replace with test Available Port Util
 
     @Before
     public void setUp(TestContext context) {
@@ -53,7 +53,7 @@ public class VerticleTest {
     public void testVerticle(TestContext context) {
         final Async async = context.async();
 
-        vertx.createHttpClient().getNow(port, "localhost", "/",
+        vertx.createHttpClient().getNow(port, "localhost", "/greetings/Christian",
                 response -> {
                     response.handler(body -> {
                         context.assertTrue(body.toString().contains("Hello"));

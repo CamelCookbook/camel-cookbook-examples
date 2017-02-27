34dca7a52425bf6c7fd6663a34f97ab7748edb6e
Scott Cranton <scott@cranton.com>
2017-02-27 07:45:55 -0500

    Resolves camel-cookbook-examples-66 Create unit tests for MenuService

--- camel-cookbook-rest/src/main/java/org/camelcookbook/rest/common/MenuItem.java
+++ camel-cookbook-rest/src/main/java/org/camelcookbook/rest/common/MenuItem.java
@@ -17,6 +17,7 @@
 
 package org.camelcookbook.rest.common;
 
+import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
@@ -35,7 +36,13 @@ public class MenuItem {
     private String description;
 
     public MenuItem() {
+    }
 
+    public MenuItem(@NotNull MenuItem item) {
+        this.id = item.id;
+        this.name = item.name;
+        this.cost = item.cost;
+        this.description = item.description;
     }
 
     public MenuItem(int id, String name, int cost, String description) {
@@ -75,5 +82,37 @@ public class MenuItem {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MenuItem menuItem = (MenuItem) o;
+
+        if (id != menuItem.id) return false;
+        if (cost != menuItem.cost) return false;
+        if (name != null ? !name.equals(menuItem.name) : menuItem.name != null) return false;
+        return description != null ? description.equals(menuItem.description) : menuItem.description == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + cost;
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "MenuItem{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", cost=" + cost +
+                ", description='" + description + '\'' +
+                '}';
     }
 }

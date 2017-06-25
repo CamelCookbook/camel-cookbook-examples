a55d3e66fd219018d159f67a97224ddf22f2ac91
Scott Cranton <scott@cranton.com>
2017-06-25 10:47:47 -0400

    Cleanup example to match book

--- camel-cookbook-split-join/src/main/java/org/camelcookbook/splitjoin/splitxml/SplitXmlNamespacesRouteBuilder.java
+++ camel-cookbook-split-join/src/main/java/org/camelcookbook/splitjoin/splitxml/SplitXmlNamespacesRouteBuilder.java
@@ -23,11 +23,11 @@ import org.apache.camel.builder.xml.Namespaces;
 class SplitXmlNamespacesRouteBuilder extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-        Namespaces namespaces = new Namespaces("c", "http://camelcookbook.org/schema/books")
+        Namespaces ns = new Namespaces("c", "http://camelcookbook.org/schema/books")
             .add("se", "http://camelcookbook.org/schema/somethingElse");
 
         from("direct:in")
-            .split(namespaces.xpath("/c:books/c:book[@category='Tech']/c:authors/c:author/text()"))
+            .split(ns.xpath("/c:books/c:book[@category='Tech']/c:authors/c:author/text()"))
                 .to("mock:out")
             .end();
     }
